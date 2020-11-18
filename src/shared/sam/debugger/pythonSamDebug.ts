ce94b9592d92322dc337b012aeb711c5025d9d4e
Richard Li <richali@amazon.com>
2020-11-18 15:57:09 -0800

    don't use python heartbeat thing

--- src/shared/sam/debugger/pythonSamDebug.ts
+++ src/shared/sam/debugger/pythonSamDebug.ts
@@ -19,7 +19,7 @@ import { getLocalRootVariants } from '../../utilities/pathUtils'
 import { Timeout } from '../../utilities/timeoutUtils'
 import { ChannelLogger } from '../../utilities/vsCodeUtils'
 import { DefaultSamLocalInvokeCommand, WAIT_FOR_DEBUGGER_MESSAGES } from '../cli/samCliLocalInvoke'
-import { invokeLambdaFunction, makeInputTemplate } from '../localLambdaRunner'
+import { invokeLambdaFunction, makeInputTemplate, waitForDebugPort } from '../localLambdaRunner'
 import { SamLaunchRequestArgs } from './awsSamDebugger'
 import { join } from 'path'
 import { ext } from '../../extensionGlobals'
@@ -128,7 +128,7 @@ export async function invokePythonLambda(
 ): Promise<PythonDebugConfiguration> {
     config.samLocalInvokeCommand = new DefaultSamLocalInvokeCommand(ctx.chanLogger, [WAIT_FOR_DEBUGGER_MESSAGES.PYTHON])
     // eslint-disable-next-line @typescript-eslint/unbound-method
-    config.onWillAttachDebugger = waitForPythonDebugAdapter
+    config.onWillAttachDebugger = waitForDebugPort
     const c = (await invokeLambdaFunction(ctx, config, async () => {})) as PythonDebugConfiguration
     return c
 }
